79c8476a25341e6bb3f1ec01644ee6b3233d73de
danielstaal <daniel.staal@hotmail.com>
2017-04-28 15:57:36 +0200

    first

---
+++ Grid.py
@@ -0,0 +1,125 @@
+import math
+from random import randint
+import numpy as np
+import copy
+
+class Grid:
+	def __init__(self, length, width, blocks):
+		self.blocks = blocks
+		self.no_of_different_blocks = len(self.blocks)
+		self.length = length
+		self.width = width
+		self.grid = np.zeros((length, width))
+		self.current_location = [0,0]
+		self.no_of_blocks = 0
+		# count the blocks of each size
+		for block in blocks: 
+			self.no_of_blocks += len(block)
+
+		self.side = 0
+
+	def add_block(self, tries):
+		new_worlds = []
+		complete_worlds = []
+
+		# TODO: FIX PROBABLY PROBLEM WITH INDEXING OF TRIES
+
+		for i in range(tries, self.no_of_different_blocks):
+			# check if not out of bounds because of removing a list in
+			# place_in_grid and decreasing self.no_of_different_blocks
+			if i < self.no_of_different_blocks:
+				# print(self.blocks)
+				flag = self.place_in_grid(self.blocks[i][0], i)
+				if flag == 0 or flag == 1:
+					return flag
+		return 2
+
+	def place_in_grid(self, block, i):
+		x = self.current_location[0]
+		y = self.current_location[1]
+		x2 = x+block[0]
+		y2 = y+block[1]
+
+		if self.side == 1:
+			y -= block[1]
+			y2 -= block[1]
+		if self.side == 2:
+			y -= block[1]-1
+			y2 -= block[1]-1
+			x -= block[0]
+			x2 -= block[0]
+
+		# print('coors:', x, x2, y, y2)
+		# print(block)
+		# print('before:\n', self.grid)
+		if(x >= 0 and x2 < self.length+1 and y >= 0 and y2 < self.width+1):
+			if not self.update_grid(x, x2, y, y2, block):
+				return 2
+			# block succesfully put in grid then:
+			else:
+				if len(self.blocks[i]) == 1:
+					del self.blocks[i]
+					self.no_of_different_blocks -= 1
+				else:
+					self.blocks[i].remove(block)
+					self.no_of_blocks -= 1
+				# and always update
+				return self.update_current_location()
+		# else:
+			# print("out of grid")
+
+	def update_grid(self, x, x2, y, y2, block):
+		# check overlap
+		# print(self.grid)
+		# print('coors:', x, x2, y, y2)		# 
+		if np.amax(self.grid[x:x2, y:y2]) > 0:
+			# print('overlap')
+			return False
+		else:
+			# print('coors:', x, x2, y, y2)
+			self.grid[x:x2, y:y2] += block[0]
+			# print('after: \n',self.grid)	
+			# print(self.grid)
+			return True
+		
+	def update_current_location(self):
+		# print(self.grid)
+		while self.side < 4:
+			# print('side:', self.side)
+			side_vector = []
+			if self.side == 0:
+				side_vector = self.grid[0, :]
+			elif self.side == 1:
+				side_vector = self.grid[:, -1]
+			elif self.side == 2:
+				side_vector = self.grid[-1, :]
+			elif self.side == 3:
+				side_vector = self.grid[:, 0]
+			# print(side_vector)	
+			# print(self.current_location)	
+			locate_zero = np.where(side_vector==0)
+			if len(locate_zero[0]) > 0: 
+				loc = 0
+				# take the last index for the bottom side
+				if self.side == 2:
+					loc = locate_zero[0][-1]
+				else:
+					loc = locate_zero[0][0]
+				self.current_location = self.get_location(loc)
+				# print('newloc:', self.current_location)
+				return 0
+			else:
+				# print(len(locate_zero[0]))
+				self.side += 1
+		return 1
+
+	def get_location(self, index):
+		if self.side == 0:
+			return [0, index]
+		if self.side == 1:
+			return [index, self.width]
+		if self.side == 2:
+			return [self.length, index]
+		else:
+			return [index, 0]
+		
---
+++ main.py
@@ -0,0 +1,119 @@
+'''
+Main running file
+Date: 
+Daniel Staal
+
+
+'''
+
+import math
+import copy
+import time
+from itertools import repeat
+from itertools import permutations
+from random import randint
+from random import shuffle
+import sys
+import csv
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import numpy as np
+import copy
+
+import Grid
+
+def solve_borders(all_blocks):
+	emptygrid = Grid.Grid(20,20, all_blocks)
+	worlds = [emptygrid]
+	complete_worlds = []
+
+	# for i in range(20):
+	i = 0
+	while len(worlds) != 0:
+		i += 1
+		print('block no: ', i)
+		[worlds, complete] = expand_worlds(worlds) 
+
+		if len(complete) > 0:
+			complete_worlds.extend(complete)
+
+	return [worlds, complete_worlds]
+
+def expand_worlds(worlds):
+	new_complete = []
+	new_worlds = []
+	for possible_world in worlds:
+		# print(possible_world.grid)
+		# print(possible_world.blocks)
+		tries = 0
+		max_tries = len(possible_world.blocks)
+		# flag == 0 means a block is placed in a new world
+		# flag == 1 means a block is placed and border is filled
+		# flag == 2 means no more options and not complete
+		flag = 0
+		while flag == 0 and tries < max_tries+1:
+			# print('tries:', tries)
+			world_copy = copy.deepcopy(possible_world)
+			flag = world_copy.add_block(tries)
+			# print('flag:',flag)
+			if flag == 0:
+				new_worlds.append(world_copy)
+			tries += 1
+		if flag == 1:
+			new_complete.append(world_copy)
+		# if flag == 2:
+			# print('no more possibilities')
+			# print(possible_world.grid)
+
+	# remove mirrored worlds
+	new_worlds = check_mirrors(new_worlds)
+	new_complete = check_mirrors(new_complete)
+
+	return [new_worlds, new_complete]
+
+def check_mirrors(worlds):
+
+	non_mirror_worlds = []
+	no_of_worlds = len(worlds)
+	# get the actual grids
+	grids = []
+	for i, world in enumerate(worlds):
+		grids.append(world.grid.all())
+
+	while len(worlds) > 0:
+		world = worlds.pop(0)
+		non_mirror_worlds.append(world)
+
+		mirrors = [world.grid, np.fliplr(world.grid), np.flipud(world.grid), np.fliplr(np.flipud(world.grid))]
+		# remove the mirrors from new_worlds
+		for i in range(len(mirrors)):
+			for j, other_world in enumerate(worlds):
+				if (other_world.grid==mirrors[i]).all():
+					del worlds[j]
+	print('Deleted mirrors:', no_of_worlds - len(non_mirror_worlds))
+	return non_mirror_worlds
+
+if __name__ == '__main__':
+	# initializing the blocks
+	# set_of_blocks = [[1,1],[2,2],[4,4], [3,3], [5,5], [2,8], [1,1]]
+	set_of_blocks = [[[1,1], [1,1]], [[8,8], [8,8]], [[9,9], [9,9]], [[4,4],[4,4]], [[5,5], [5,5], [5,5]], [[6,6], [6,6], [6,6]]]
+	# set_of_blocks = [[1,1]]*5
+	# set_of_blocks.append([[2,2]]*5)
+	# print(set_of_blocks)
+	[no_solutions, solutions] = solve_borders(set_of_blocks)
+	print(no_solutions)
+	print(solutions)
+	print('no of non-solutions: ', len(no_solutions))
+	
+	# for world in no_solutions:
+	# 	print(world.grid)
+
+	print('no of solutions: ', len(solutions))
+	my_cmap = cm.get_cmap('YlGnBu')
+	my_cmap.set_bad('white')
+
+	for i, sol in enumerate(solutions[0:10]):
+		plt.figure(i+1)
+		plt.imshow(sol.grid, cmap=my_cmap, interpolation='none')
+	plt.show()
+	
